5ed2a9ad13818d63092ecec071824bd10eddbd9b
kshuta <shutakumada@gmail.com>
2020-08-13 22:34:01 +0900

    Changed post_detail.html to look better

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import (IndexView, PostDetailView, CategoryListView, TagListView
 
 app_name = 'blog'
 urlpatterns = [
-	path('', IndexView.as_view(), name='index'),
+	path('', IndexView.as_view(), name='root'),
 	path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 	path('categories/', CategoryListView.as_view(), name='category_list'),
 	path('tags/', TagListView.as_view(), name='tag_list'),
